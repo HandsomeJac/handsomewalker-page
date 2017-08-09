3e723fb5466d32d4f563a6d795cdc6fc5f8bb2f1
handsomeJac <570926839@qq.com>
2017-08-10 00:23:53 +0800

    get data from mongodb

--- src/app/art-area/art-area.component.ts
+++ src/app/art-area/art-area.component.ts
@@ -6,19 +6,31 @@ import { MyDataService } from '../services/my-data.service';
   selector: 'app-art-area',
   templateUrl: './art-area.component.html',
   styleUrls: ['./art-area.component.css'],
-  providers: [],
+  providers: [MyDataService],
 })
 
 export class ArtAreaComponent implements OnInit {
+  articles;
+  is_collapsed_arr = [];
   constructor(private _myDataService: MyDataService, private _broadcaster: Broadcaster) {
   }
-
   ngOnInit() {
+    this.getData('art');
   }
-  getData(type: string) {
+  /* 处理collapse数组 */
+  handleCollapse(data): void {
+    for (let i = 0, len = data.length; i < len; i++) {
+      this.is_collapsed_arr.push(true);
+    }
+  }
+  /* 获取数据 */
+  getData(type: string): void {
     this._myDataService.getArticleData(type).subscribe(res => {
-      console.log(res);
-    }, error => console.log(error));
+      if (res['status'] === 200) {
+        this.articles = res['data']['articles'];
+        this.handleCollapse(this.articles);
+      }
+    }, error => console.debug(error));
   }
 
 }
--- src/app/services/my-data.service.ts
+++ src/app/services/my-data.service.ts
@@ -7,23 +7,23 @@ export class MyDataService {
   private proxy_url;
   constructor(private http: Http) {
     this.proxy_url = 'http://127.0.0.1:7777';
-   }
+  }
 
-  private _handleUrl(type){
+  private _handleUrl(type) {
     const data = {
       'file': `${type}article.json`
     };
     const json_data = JSON.stringify(data);
     return json_data;
   }
-  
+
   /* 请求文章数据 */
-  getArticleData(type){
+  getArticleData(type) {
     const param = new URLSearchParams();
     param.set('db', 'article_db');
     param.set('collection', type);
-    const _res = this.http.get(this.proxy_url, {search: param})
-    .map((res) => res.json());
+    const _res = this.http.get(this.proxy_url, { search: param })
+      .map((res) => res.json());
     return _res;
   }
 
